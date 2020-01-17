c6aa9fbfb0e5654c5f8dc096fd8706ae14dcbb4b
Josh Johnson <josh@joshajohnson.com>
2020-01-17 12:15:51 +1100

    setup no longer overwrites version

--- setup.sh
+++ setup.sh
@@ -14,6 +14,11 @@ if [ $# -ne 3 ]; then
     echo "Usage: $0 <version> <project_name> <company_name>" > /dev/stderr
     exit 1
 fi
+
+if [ -d "../hardware/$VERSION" ]; then
+	echo "+++ Version exists, please enter unique version!"
+	exit 1
+fi
  
 ORIG_COMPANY="Josh Johnson"
  
